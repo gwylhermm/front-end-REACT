b106092770250d922f2b227abf9710df44e7f336
Gwylhermm <gwylhermrrodrigues@gmail.com>
2026-09-08 22:13:29 -0300

    atualização da aula 04

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -3,7 +3,7 @@ export default function HabitCard({
   title,
   goal,
   completed,
-  onShowDetails,
+  onToggle,
 }) {
   return (
     <article className={`habit-card ${completed ? "is-complete" : ""}`}>
@@ -11,9 +11,9 @@ export default function HabitCard({
         <h2>{title}</h2>
         <p>Meta: {goal}</p>
       </div>
-     
-      <button type="button" onClick={() => onShowDetails(id)}>
-        ver detalhes
+
+      <button type="button" onClick={() => onToggle(id)}>
+        {completed ? "Desmarcar" : "Concluir"}
       </button>
     </article>
   );
